21e93278c8521bb8bb9bc24ff642717e48ca0f3c
LinkinPony <friendshipgame714@gmail.com>
2023-03-12 10:42:12 +0800

    finish simple single queue thread pool

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <benchmark/benchmark.h>
+#include <thread_pool.h>
 // 15_parallel_algorithm.cpp
 using namespace std;
 const int kTaskNum = 20;
 void time_consume_func(){
     int siz = 2000;
     std::vector<int>vec(siz,0);
-    for(int i = 1;i <= 100;i++) {
+    for(int i = 1;i <= 10000;i++) {
         int x = rand() * i + 10;
         int idx = (i * x) % siz;
         idx = abs(idx) % siz;
@@ -20,6 +21,7 @@ void default_run(int n){
     }
 }
 void naive_thread_run(int n){
+
     std::vector<std::thread>threads;
     for(int i = 1;i <= n;i++){
         threads.emplace_back(time_consume_func);
@@ -27,6 +29,21 @@ void naive_thread_run(int n){
     for(auto & it:threads){
         it.join();
     }
+}
+void f(){
+  std::cout << "qwq" << std::endl;
+}
+ThreadPool::ThreadPool thread_pool(12);
+void thread_pool_run(int n){
+
+  std::vector<future<decltype(time_consume_func())>>futures;
+  for(int i = 1;i <= n;i++){
+    futures.emplace_back(thread_pool.submit(time_consume_func));
+  }
+  for(auto & it:futures){
+    it.get();
+  }
+//  thread_pool.stop();
 }
 
 static void BM_default_run(benchmark::State & state){
@@ -41,7 +58,14 @@ static void BM_naive_thread_run(benchmark::State & state){
         naive_thread_run(kTaskNum);
     }
 }
-
 BENCHMARK(BM_naive_thread_run);
+
+
+static void BM_thread_pool_run(benchmark::State & state){
+  for(auto _:state){
+    thread_pool_run(kTaskNum);
+  }
+}
+BENCHMARK(BM_thread_pool_run);
 BENCHMARK_MAIN();
 
--- thread_pool.h
+++ thread_pool.h
@@ -10,10 +10,112 @@
 #include <thread>
 #include <chrono>
 #include <queue>
+#include <vector>
+#include <functional>
+
+namespace ThreadPool {
+template<typename T>
+class ThreadSafeQueue {
+ public:
+  size_t size();
+  T &front();
+  void pop();
+  void push(T &in);
+  void clear();
+ private:
+  std::queue<T> queue_;
+  std::mutex mutex_;
+};
+template<typename T>
+void ThreadSafeQueue<T>::clear() {
+  std::unique_lock<std::mutex> lock{mutex_};
+  while (!queue_.empty()) {
+    queue_.pop();
+  }
+}
+template<typename T>
+void ThreadSafeQueue<T>::pop() {
+  std::unique_lock<std::mutex> lock{mutex_};
+  queue_.pop();
+}
+template<typename T>
+T &ThreadSafeQueue<T>::front() {
+  std::unique_lock<std::mutex> lock{mutex_};
+  return queue_.front();
+}
+template<typename T>
+void ThreadSafeQueue<T>::push(T &in) {
+  std::unique_lock<std::mutex> lock{mutex_};
+  queue_.emplace(in);
+}
+template<typename T>
+size_t ThreadSafeQueue<T>::size() {
+  std::unique_lock<std::mutex> lock{mutex_};
+  return queue_.size();
+}
+//extract tasks in queue and handle them. each worker is a endless thread.
+
 
 class ThreadPool {
+ public:
+  //submit a task, return the future of this task.
+  template<typename F, typename... Args>
+  auto submit(F &&f, Args &&... args) -> std::future<decltype(f(args...))>;
+ public:
+  explicit ThreadPool(size_t thread_num = std::thread::hardware_concurrency());
+  ~ThreadPool();
+  ThreadPool(const ThreadPool &) = delete;
+  ThreadPool(ThreadPool &&) = delete;
+  ThreadPool& operator = (const ThreadPool &) = delete;
+  ThreadPool& operator = (ThreadPool&&) = delete;
+ public:
+  [[nodiscard]] bool isRun() const { return run_; }
+  //Terminate and discard all queued works
+  void stop();
+ public:
+  std::mutex worker_mutex_;
+  std::condition_variable queue_cond_;
+  ThreadSafeQueue<std::function<void()>> task_queue_;
+ private:
+  void init();
+ private:
+  bool run_;
+  int thread_num_;
+  std::vector<std::thread> threads_;
+
 
 };
-
-
+class ThreadWorker {
+ public:
+  ThreadWorker(ThreadPool *pool, int id);
+  void work();
+  void operator()();
+ private:
+  void run();
+ private:
+  int id_;
+  //we don't care how to manage pool's resource in worker, so just use bare pointer.
+  ThreadPool *pool_;
+};
+template<typename F, typename... Args>
+auto ThreadPool::submit(F &&f, Args &&... args) -> std::future<decltype(f(args...))> {
+  //bind origin function into func. now we can finish task by simply call func().
+  auto func = std::bind(
+      std::forward<F>(f),
+      std::forward<Args>(args)...
+  );
+  using kTaskType = std::packaged_task<decltype(f(args...))()>;
+  auto task_ptr = std::make_shared<kTaskType>(func);
+  //use closure to capture task pointer and wrap it into void function.
+  //so we can process these task in a queue easily.
+  auto closure = std::function<void()>(
+      [task_ptr] {
+        (*task_ptr)();
+      }
+  );
+  task_queue_.push(closure);
+  queue_cond_.notify_one();
+  return task_ptr->get_future();
+}
+};
 #endif //SIMPLETHREADPOOL_THREAD_POOL_H
